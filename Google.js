6184dd625ef077530a2e6cb3263ef288f99cfad4
I T_code <oluwagbotemioyeboade@gmail.com>
2023-01-07 01:26:42 +0100

    Google js script

---
+++ Google.js
@@ -0,0 +1,10 @@
+function change(){
+	var el = document.getElementsByClassName("form-container");
+for(x =0; x<=el.length; x++){
+	el[0].innerHTML="To every greatness there is a goal";
+el[0].style.color="white"
+el[0].style.width="250px"
+
+}
+
+}
